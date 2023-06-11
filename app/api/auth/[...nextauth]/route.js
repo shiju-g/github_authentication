6fd45fe67cf48bfb862acfd25de76842e84514db
SHIJU G <85386417+shiju-g@users.noreply.github.com>
2023-06-11 12:52:40 +0530

    github authentication

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,29 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions = NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user.id = token.id;
+      session.accessToken = token.accessToken;
+      return session;
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      if (user) {
+        token.id = user.id;
+      }
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+  },
+});
+
+export { authOptions as GET, authOptions as POST };
