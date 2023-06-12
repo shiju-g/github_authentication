40f42a7ca6d0dc23de969d0b94560e9f6693ed64
SHIJU G <85386417+shiju-g@users.noreply.github.com>
2023-06-12 22:36:40 +0530

    Header component for dashboard

---
+++ components/DashboardHeader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DashboardHeader = () => {
+  return (
+    <header className="p-5 fixed z-50 top-0 left-0 w-full shadow  bg-white ">
+      <h1 className="font-semibold text-xl text-center">
+        Welcome to Dashboard
+      </h1>
+    </header>
+  );
+};
+
+export default DashboardHeader;
